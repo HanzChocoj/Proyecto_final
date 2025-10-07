37a853f57092c90812f2d147b46616657d04fd98
HanzChocoj <kchocojx@miumg.edu.gt>
2025-10-06 21:14:34 -0600

    Instalacion y conecion con mysql

--- proyectof/settings.py
+++ proyectof/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'proyectof.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'proyecto',
+        'USER':'root',
+        'PASSWORD':'Gameover',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+      
     }
 }
 
