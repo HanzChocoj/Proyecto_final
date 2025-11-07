d5b544ba4e806386146ad0d32a92cf700b76a873
HanzChocoj <kchocojx@miumg.edu.gt>
2025-11-07 15:46:27 -0600

    Correccion de HTTPS

--- proyectof/settings.py
+++ proyectof/settings.py
@@ -59,7 +59,7 @@ if USE_RAILWAY:
 
     # --- Seguridad HTTPS ---
     SECURE_PROXY_SSLHEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True") == "True"
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
